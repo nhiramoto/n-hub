8c5ff93c06a25e82b879a2abc228b5ccc05ac0a1
nhtoshiaki <nh_toshi.94@hotmail.com>
2017-07-10 19:08:30 -0300

    Testing welcome page layout.

---
+++ script/welcome.js
@@ -0,0 +1,7 @@
+document.getElementById('btn1').addEventListener('click', () => {
+    document.getElementById('slider').setAttribute('class', 'second');
+});
+
+document.getElementById('btn2').addEventListener('click', () => {
+    document.getElementById('slider').setAttribute('class', '');
+});
