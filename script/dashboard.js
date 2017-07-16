ce85efa25d403727f77f60b572bc1bfb0eacbdbc
nhtoshiaki <nh_toshi.94@hotmail.com>
2017-07-16 17:57:28 -0300

    Changing menu with sidebar buttons.

--- script/dashboard.js
+++ script/dashboard.js
@@ -12,4 +12,25 @@ $(document).ready(() => {
         $('#sidebar').toggleClass('hidden');
         $('#showSidebar').toggleClass('hidden');
     });
+
+    $('#emailBtn').click((event) => {
+        console.log('E-mail!!!');
+        $('.resMenu.active').removeClass('active');
+        $('#emailMenu').addClass('active');
+    });
+    $('#chatBtn').click((event) => {
+        console.log('Chat!!!');
+        $('.resMenu.active').removeClass('active');
+        $('#chatMenu').addClass('active');
+    });
+    $('#calendarBtn').click((event) => {
+        console.log('Calendário!!!');
+        $('.resMenu.active').removeClass('active');
+        $('#calendarMenu').addClass('active');
+    });
+    $('#newsBtn').click((event) => {
+        console.log('Notícias!!!');
+        $('.resMenu.active').removeClass('active');
+        $('#newsMenu').addClass('active');
+    });
 });
